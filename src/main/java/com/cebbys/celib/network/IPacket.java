8238b83fad8ce9193463e2a6dbcc83d9fdc1c094
Domaman202 <vip.domaman@mail.ru>
2020-12-31 08:09:36 +0300

    .

--- src/main/java/com/cebbys/celib/network/IPacket.java
+++ src/main/java/com/cebbys/celib/network/IPacket.java
@@ -3,7 +3,7 @@ package com.cebbys.celib.network;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
-public interface IPackage {
+public interface IPacket {
     void read(PacketByteBuf buf);
     void write(PacketByteBuf buf);
     Identifier getID();
--- src/main/kotlin/com/cebbys/celib/utilities/GroovyHelper.kt
+++ src/main/kotlin/com/cebbys/celib/utilities/GroovyHelper.kt
@@ -4,11 +4,36 @@ import groovy.lang.Binding
 import groovy.lang.GroovyClassLoader
 import groovy.lang.GroovyCodeSource
 import groovy.lang.GroovyShell
+import net.minecraft.util.Pair
 import org.codehaus.groovy.control.CompilerConfiguration
 import java.io.File
 
 class GroovyHelper : GroovyShell {
     constructor(parent: ClassLoader? = null, binding: Binding? = Binding(), config: CompilerConfiguration = CompilerConfiguration.DEFAULT) : super(parent, binding, config)
+
+    /**
+     * @param props Array of pairs of names and values
+     */
+    fun setProperties(props: Array<Pair<String, Any?>>) = props.forEach{ element -> setProperty(element.left, element.right) }
+
+    /**
+     * @param names Array with names
+     */
+    fun removeProperties(names: Array<String>) = names.forEach { name -> context.variables.remove(name) }
+
+    /**
+     * @param names Array with names
+     */
+    fun getProperties(names: Array<String>): ArrayList<Any?> {
+        val values = ArrayList<Any?>()
+        names.forEach { name -> values.add(getProperty(name)) }
+        return values
+    }
+
+    /**
+     * @param name Name of object for removing
+     */
+    fun removeProperty(name: String) = context.variables.remove(name)
 
     fun parseClassFile(path: String) = classLoader.parseClass(File(path))
     fun parseClassFile(file: File)   = classLoader.parseClass(file)
