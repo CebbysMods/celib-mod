afeb6ff2aa9314262c46f5549647405ffedbc5a9
CebbyS <cebbys.dev@gmail.com>
2021-01-03 18:06:49 +0200

    Small shanges

--- src/main/java/com/cebbys/celib/utilities/CelibBlockPos.java
+++ src/main/java/com/cebbys/celib/utilities/CelibBlockPos.java
@@ -6,42 +6,53 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 
 public class CelibBlockPos extends BlockPos {
-    public CelibBlockPos(int i, int j, int k)
-    { super(i, j, k); }
-    public CelibBlockPos(double d, double e, double f)
-    { super(d, e, f); }
-    public CelibBlockPos(Vec3d pos)
-    { super(pos); }
-    public CelibBlockPos(Position pos)
-    { super(pos); }
-    public CelibBlockPos(Vec3i pos)
-    { super(pos); }
+	public CelibBlockPos(int i, int j, int k) {
+		super(i, j, k);
+	}
 
-    public BlockPos add( BlockPos p ) {
-        setX(getX() + p.getX());
-        setY(getY() + p.getY());
-        setZ(getZ() + p.getZ());
+	public CelibBlockPos(double d, double e, double f) {
+		super(d, e, f);
+	}
 
-        return this;
-    }
-    public BlockPos sub( BlockPos p ) {
-        setX(getX() - p.getX());
-        setY(getY() - p.getY());
-        setZ(getZ() - p.getZ());
+	public CelibBlockPos(Vec3d pos) {
+		super(pos);
+	}
 
-        return this;
-    }
+	public CelibBlockPos(Position pos) {
+		super(pos);
+	}
 
-    public Vec3d toVec3d()
-    { return new Vec3d( getX(), getY(), getZ() ); }
-    public Vec3i toVec3i()
-    { return new Vec3i( getX(), getY(), getZ() ); }
+	public CelibBlockPos(Vec3i pos) {
+		super(pos);
+	}
 
-    public static CelibBlockPos ofVec3d(Vec3d vec) {
-        return new CelibBlockPos(vec.x, vec.y, vec.z);
-    }
+	public BlockPos add(BlockPos p) {
+		setX(getX() + p.getX());
+		setY(getY() + p.getY());
+		setZ(getZ() + p.getZ());
+		return this;
+	}
 
-    public static CelibBlockPos ofVec3i(Vec3d vec) {
-        return new CelibBlockPos(vec.x, vec.y, vec.z);
-    }
+	public BlockPos sub(BlockPos p) {
+		setX(getX() - p.getX());
+		setY(getY() - p.getY());
+		setZ(getZ() - p.getZ());
+		return this;
+	}
+
+	public Vec3d toVec3d() {
+		return new Vec3d(getX(), getY(), getZ());
+	}
+
+	public Vec3i toVec3i() {
+		return new Vec3i(getX(), getY(), getZ());
+	}
+
+	public static CelibBlockPos ofVec3d(Vec3d vec) {
+		return new CelibBlockPos(vec.getX(), vec.getY(), vec.getZ());
+	}
+
+	public static CelibBlockPos ofVec3i(Vec3i vec) {
+		return new CelibBlockPos(vec.getX(), vec.getY(), vec.getZ());
+	}
 }
